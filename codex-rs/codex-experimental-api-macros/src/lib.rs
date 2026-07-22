470d2b9e1913aacb43ab50cffb1f860680e3188a
weiyuhl <weiyuhl@users.noreply.github.com>
2026-07-23 07:29:53 +0800

    物理彻底删除 connectors Crate, codex-experimental-api-macros Crate 与 ext/goal 扩展模块 (-4,500 LoC)

--- codex-rs/codex-experimental-api-macros/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use proc_macro::TokenStream;
-
-#[proc_macro_derive(ExperimentalApi, attributes(experimental))]
-pub fn derive_experimental_api(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
-}
